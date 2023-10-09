3cdf508896967bfcdc0ac92f101731972074241d
Szymon Półtorak <szymonpotorak@gmail.com>
2023-10-09 21:37:21 +0200

    Initial commit

---
+++ library-backend/src/test/java/pl/edu/pw/ee/library/LibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.ee.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
