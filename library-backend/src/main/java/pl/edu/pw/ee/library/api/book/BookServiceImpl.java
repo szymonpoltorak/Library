7a6217d1a1ff1bb0c5388fe85728d243599b633d
JakubNiewadzi <01169600@pw.edu.pl>
2023-10-24 17:04:07 +0200

    removed annotations not in use

--- library-backend/src/main/java/pl/edu/pw/ee/library/api/book/BookServiceImpl.java
+++ library-backend/src/main/java/pl/edu/pw/ee/library/api/book/BookServiceImpl.java
@@ -1,6 +1,5 @@
 package pl.edu.pw.ee.library.api.book;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
