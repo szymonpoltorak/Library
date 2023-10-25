d96d637fe6938dd0761f00bce3c0ee5e6eeb2774
JakubNiewadzi <01169600@pw.edu.pl>
2023-10-25 22:17:43 +0200

    refactoring and new exceptions

--- library-backend/src/main/java/pl/edu/pw/ee/library/api/book/validators/BookRequestValidator.java
+++ library-backend/src/main/java/pl/edu/pw/ee/library/api/book/validators/BookRequestValidator.java
@@ -1,38 +1,42 @@
 package pl.edu.pw.ee.library.api.book.validators;
 
 import pl.edu.pw.ee.library.api.book.data.BookRequest;
+import pl.edu.pw.ee.library.exceptions.book.BlankStringException;
+import pl.edu.pw.ee.library.exceptions.book.InvalidBooksInStockException;
+import pl.edu.pw.ee.library.exceptions.book.NullRequestException;
+import pl.edu.pw.ee.library.exceptions.book.NullStringException;
 
 import static pl.edu.pw.ee.library.api.book.validators.constants.ValidatorConstants.*;
 
 public final class BookRequestValidator {
 
-    private BookRequestValidator(){
+    private BookRequestValidator() {
     }
 
-    public static void validateBookRequest(BookRequest bookRequest){
-        if(bookRequest==null){
-            throw new IllegalArgumentException(BOOK_REQUEST_NULL);
+    public static void validateBookRequest(BookRequest bookRequest) {
+        if (bookRequest == null) {
+            throw new NullRequestException(BOOK_REQUEST_NULL);
         }
         validateString(bookRequest.author());
         validateString(bookRequest.title());
         validateBooksInStock(bookRequest.booksInStock());
     }
 
-    private static void validateString(String string){
-        if(string==null){
-            throw new IllegalArgumentException(STRING_NULL);
+    private static void validateString(String string) {
+        if (string == null) {
+            throw new NullStringException(STRING_NULL);
         }
-        if(string.isBlank()){
-            throw new IllegalArgumentException(STRING_EMPTY_BLANK);
+        if (string.isBlank()) {
+            throw new BlankStringException(STRING_EMPTY_BLANK);
         }
     }
 
-    private static void validateBooksInStock(int booksInStock){
-        if(booksInStock<1){
-            throw new IllegalArgumentException(TOO_FEW_BOOKS);
+    private static void validateBooksInStock(int booksInStock) {
+        if (booksInStock < 1) {
+            throw new InvalidBooksInStockException(TOO_FEW_BOOKS);
         }
-        if(booksInStock>100){
-            throw new IllegalArgumentException(TOO_MANY_BOOKS);
+        if (booksInStock > 100) {
+            throw new InvalidBooksInStockException(TOO_MANY_BOOKS);
         }
     }
 
