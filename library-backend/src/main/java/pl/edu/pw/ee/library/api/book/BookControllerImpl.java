993ecd211d5fc1b17244fa93c478dca2ef16fb7c
Szymon Półtorak <szymonpotorak@gmail.com>
2023-11-05 12:03:09 +0100

    Fixed syntax error in docker compose

--- library-backend/src/main/java/pl/edu/pw/ee/library/api/book/BookControllerImpl.java
+++ library-backend/src/main/java/pl/edu/pw/ee/library/api/book/BookControllerImpl.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.ee.library.api.book;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
